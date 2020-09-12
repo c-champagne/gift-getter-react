7c48b5c29ec17ead181d27f5be783383494660bd
c-champagne <cassielchampagne@gmail.com>
2020-09-12 11:20:39 -0400

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import {Switch,
+        Route,
+        Link} from 'react-router-dom';
+
+export class Home extends Component {
+
+    render() {
+
+        return (
+            <div> 
+                <Switch>
+                    
+                </Switch>
+            </div>
+        )
+
+
+
+    }
+
+
+
+}
